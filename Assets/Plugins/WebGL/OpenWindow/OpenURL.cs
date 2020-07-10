d857d415951602390b0d2b40293e270c7d6032b9
kou_yeung <kou_yeung@hotmail.com>
2020-07-10 09:40:21 +0900

    リファクタリング

---
+++ Assets/Plugins/WebGL/OpenWindow/OpenURL.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
+
+public class OpenURL : MonoBehaviour , IPointerDownHandler
+{
+    public string url;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
+#else
+    private static void openWindow(string url) { Debug.Log(string.Format("openWindow:{0}", url)); }
+#endif
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!string.IsNullOrEmpty(url))
+        {
+            openWindow(url);
+        }
+    }
+}
